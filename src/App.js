5c932101e9377bacbd45f9a45a197205e598ad6e
Jouni Juntunen <jouni.juntunen@oamk.fi>
2022-03-01 08:50:55 +0200

    Haku ja lisäys toimii frontendissä.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost:8888/todo2022/';
 
 function App() {
+  const [task,setTask] = useState('');
+  const [tasks,setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setTasks(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }, [])
+  
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:task});
+    axios.post(URL + 'add.php',json, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      setTasks(tasks => [...tasks,response.data]);
+      setTask('');
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={save}>
+        <label>New task</label>
+        <input value={task} placeholder='Add a new task' onChange={e => setTask(e.target.value)} />
+        <button>Save</button>
+      </form>
+      <ol>
+        {tasks?.map(task =>(
+          <li key={task.id}>{task.description}</li>
+        ))}
+      </ol>
     </div>
   );
 }
